1faf29f35f252e420abcdf94fbb5cca53adf494b
Gabriel Nakamoto <gabriel@nakamoto.ca>
2025-02-26 08:09:27 -0500

    Fixed imports?

--- src/main/java/frc/robot/subsystems/vision/VisionIOPhotonVision.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIOPhotonVision.java
@@ -13,7 +13,7 @@
 
 package frc.robot.subsystems.vision;
 
-import static frc.constants.VisionConstants.*;
+import static frc.robot.constants.VisionConstants.*;
 
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
