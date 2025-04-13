a796cd397107ff2fbc0a9f1c624bd0922d7c474c
Gabriel Nakamoto <gabriel@nakamoto.ca>
2025-04-13 19:31:45 -0400

    Started algae projectile simulation

--- src/main/java/frc/robot/subsystems/algaeIntake/AlgaeIntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/algaeIntake/AlgaeIntakeIOSim.java
@@ -4,26 +4,44 @@
 
 package frc.robot.subsystems.algaeIntake;
 
-import static edu.wpi.first.units.Units.*;
+import static edu.wpi.first.units.Units.Degree;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.InchesPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 
+import java.util.List;
+
+import org.ironmaple.simulation.SimulatedArena;
+import org.ironmaple.simulation.drivesims.SwerveDriveSimulation;
+import org.ironmaple.simulation.seasonspecific.reefscape2025.ReefscapeAlgaeOnFly;
+
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.units.measure.AngularVelocity;
 import frc.robot.constants.FieldConstants.Reef;
 import frc.robot.constants.FieldConstants.Side;
 import frc.robot.constants.FieldConstants.StagingPositions;
 import frc.robot.subsystems.algaeIntake.AlgaeIntakeConstants.AlgaeIntakeAction;
-import java.util.List;
-import org.ironmaple.simulation.drivesims.SwerveDriveSimulation;
+import frc.robot.subsystems.superstructure.arm.Arm;
+import frc.robot.subsystems.superstructure.elevator.Elevator;
 
 /** Add your docs here. */
 public class AlgaeIntakeIOSim implements AlgaeIntakeIO {
     private final SwerveDriveSimulation driveSimulation;
+    private final Elevator elevator;
+    private final Arm arm;
     private AngularVelocity intakeSpeed = RotationsPerSecond.of(0);
     private boolean hasAlgae = false;
 
-    public AlgaeIntakeIOSim(SwerveDriveSimulation driveSimulation) {
+    public AlgaeIntakeIOSim(SwerveDriveSimulation driveSimulation, Elevator elevator, Arm arm) {
         this.driveSimulation = driveSimulation;
+        this.elevator = elevator;
+        this.arm = arm;
     }
 
     @Override
@@ -33,6 +51,40 @@ public class AlgaeIntakeIOSim implements AlgaeIntakeIO {
         // check for outtaking, intaking handled in update inputs
         if (targetSpeed.lt(RotationsPerSecond.of(0)) && hasAlgae) {
             // simulate projectile motion
+            Pose3d algaePose = new Pose3d();
+            Pose2d robotPose = driveSimulation.getSimulatedDriveTrainPose();
+            // algae position relative to arm pivot point
+            algaePose = algaePose.transformBy(new Transform3d(
+                    Inches.of(24.0), // outwards from elevator
+                    Inches.of(0),
+                    Inches.of(0),
+                    Rotation3d.kZero));
+
+            // roll, pitch, yaw
+            algaePose = algaePose.rotateBy(new Rotation3d(
+                    Degrees.of(0),
+                    arm.getArmPosition().times(-1).plus(Degrees.of(13)),
+                    robotPose.getRotation().getMeasure()));
+            algaePose = new Pose3d(
+                    algaePose.getMeasureX().plus(robotPose.getMeasureX()),
+                    algaePose.getMeasureY().plus(robotPose.getMeasureY()),
+                    algaePose.getMeasureZ().plus(Inches.of(21.875)).plus(elevator.getPosition()),
+                    Rotation3d.kZero);
+
+            SimulatedArena.getInstance()
+                .addGamePieceProjectile(new ReefscapeAlgaeOnFly(
+                    driveSimulation.getSimulatedDriveTrainPose().getTranslation(),
+                    arm.getArmPosition().gt(Degrees.of(0)) ? 
+                        new Translation2d(algaePose.getMeasureX().times(-1), algaePose.getMeasureY()) :
+                        new Translation2d(algaePose.getMeasureX(), algaePose.getMeasureY()),
+                    driveSimulation.getDriveTrainSimulatedChassisSpeedsFieldRelative(),
+                    arm.getArmPosition().gt(Degrees.of(0)) ?
+                        robotPose.getRotation().plus(Rotation2d.k180deg) :
+                        robotPose.getRotation(),
+                    algaePose.getMeasureZ(),
+                    // account for upwards, elevator velocity + arm velocity for barge
+                    InchesPerSecond.of(Math.PI * 2.0 * intakeSpeed.in(RotationsPerSecond) * 2.0),
+                    algaePose.getRotation().getMeasureY())); // pitch
             // just processor to start
             hasAlgae = false;
         }
